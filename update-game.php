cf720332c4a35c5831e25833e65d6457a4b9a851
unknown <belasalah@gmail.com>
2023-03-30 17:14:02 +0200

    bug not done

--- update-game.php
+++ update-game.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -10,61 +11,88 @@
 </head>
 
 <body>
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="/jeux">Games</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/jeux">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/jeux/new-game.php">Ajouter un jeu</a>
-        </li>
-        
-      </ul>
-      
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="/jeux">Games</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="/jeux">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/jeux/new-game.php">Ajouter un jeu</a>
+          </li>
+
+        </ul>
+
+      </div>
     </div>
-  </div>
-</nav>
-        
+  </nav>
+  <h1 class="text-center m-5">Modifier un jeu</h1>
+  <form action="update-game.php" method="POST">
+    <div>
+      <label for="">Nom du jeu</label>
+      <input type="text" name="nom">
+    </div>
+    <div>
+      <label for="">Possesseur</label>
+      <input type="text" name="possesseur">
+    </div>
+    <div>
+      <label for="">Prix</label>
+      <input type="number" name="prix">
+    </div>
+    <div>
+      <label for="">Nombre de joueurs</label>
+      <input type="number" name="nbr">
+    </div>
+    <div>
+      <label for="">Commentaire</label>
+      <input type="text" name="commentaires">
+    </div>
+    <div>
+      <label for="">Console</label>
+      <input type="text" name="console">
+    </div>
+
+    <div>
+      <button type="submit">Envoyer</button>
+    </div>
+  </form>
 </body>
+
 </html>
 
 <?php
-
-
-
-$nom= $_POST['nom'];
-$commentaires= $_POST['commentaire'];
-$nbr= $_POST['nbr'];
-$prix= $_POST['prix'];
-$possesseur= $_POST['possesseur'];
-$console= $_POST['console'];
-
 include './utils/db.php';
 
-$sql = "INSERT INTO
-         jeux_video(nom, possesseur,console,prix, nbre_joueurs_max,commentaires)
-        VALUES ('$nom', '$possesseur', '$console', '$prix', '$nbr', '$commentaires')";
+
+if (
+  isset($_POST['nom']) &&
+  isset($_POST['possesseur']) &&
+  isset($_POST['console']) &&
+  isset($_POST['prix']) &&
+  isset($_POST['nbr']) &&
+  isset($_POST['commentaire'])
+  
+  ) {
+    $nom = $_POST['nom'];
+    
+    $possesseur = $_POST['possesseur'];
+    $console = $_POST['console'];
+    $prix = $_POST['prix'];
+    $nbr = $_POST['nbr'];
+    $commentaires = $_POST['commentaires'];
+    
+    echo"Ok";
+    die($nom);
 
 
-include './utils/errorsHandler.php';
+}
+
+echo "Error";
 
 
-
-
-<?php
-
-
-include './utils/db.php';
-
-$sql = "UPDATE jeux_video
-        SET nom='Luigi cart', console='Nintendo',prix=50
-        WHERE ID=2 ";
-
-
-include './utils/errorsHandler.php';
